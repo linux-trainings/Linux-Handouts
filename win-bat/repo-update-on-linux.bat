079d11eeac3d59e1bceac038e2c4458236049021
Hermann Hueck <hermann.hueck@mailbox.org>
2025-02-19 17:56:20 +0100

    Update repo at Mi 19. Feb 17:56:20 CET 2025

--- win-bat/repo-update-on-linux.bat
+++ win-bat/repo-update-on-linux.bat
@@ -2,6 +2,6 @@ set user=hermann
 set host=debian.mshome.net
 
 echo Aktualisierung des Git-Kurs-Repo von %user% auf %host% ...
-ssh %user%@%host% cd linux-trainings\Linux-Handouts; git pull
+ssh %user%@%host% cd linux-trainings/Linux-Handouts; git pull
 
 pause
